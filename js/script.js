062c2d15088dad566416bae41b15d04a2513f9a7
Andrea <andrellart@hotmail.com>
2021-04-21 22:12:06 +0200

    Creado botones para acceder a puzzles
    Habilitado el botón de comenzar el juego en javascript.
    Añadido un mapa provisional y botones provisionales para acceder a los puzzles.
    Habilitado el botón en javascript para abrir los puzzles.
    Comienzo de la estructura de los puzzles en el puzzle1.

--- js/script.js
+++ js/script.js
@@ -1,6 +1,12 @@
 //INICIALIZACION
 
 $("#inst").hide();
+$("#puzzle1").hide();
+$("#puzzle2").hide();
+$("#puzzle3").hide();
+$("#puzzle4").hide();
+$("#puzzle5").hide();
+$("#puzzle6").hide();
 
 
 
@@ -11,6 +17,7 @@ $("#inicio_btn2").click(function(){
     $("#inst").show();
 
     $("#inicio").css("-webkit-filter", "blur(10px)");
+    $("#mapa").css("-webkit-filter", "blur(10px)");
 });
 
 
@@ -22,5 +29,65 @@ $("#instrucciones_btn").click(function(){
     $("#inst").hide();
 
     $("#inicio").css("-webkit-filter", "blur(0px)");
+    $("#mapa").css("-webkit-filter", "blur(0px)");
 });
+
+
+//COMENZAR JUEGO
+$("#inicio_btn1").click(function(){
+
+    $("#inicio").hide();
+
+});
+
+
+//BOTON PUZZLE 1
+$("#puzzle1_btn").click(function(){
+
+    $("#puzzle1").show();
+
+    $("#mapa").css("-webkit-filter", "blur(10px)");
+});
+
+//BOTON PUZZLE 2
+$("#puzzle2_btn").click(function(){
+
+    $("#puzzle2").show();
+
+    $("#mapa").css("-webkit-filter", "blur(10px)");
+});
+
+//BOTON PUZZLE 3
+$("#puzzle3_btn").click(function(){
+
+    $("#puzzle3").show();
+
+    $("#mapa").css("-webkit-filter", "blur(10px)");
+});
+
+//BOTON PUZZLE 4
+$("#puzzle4_btn").click(function(){
+
+    $("#puzzle4").show();
+
+    $("#mapa").css("-webkit-filter", "blur(10px)");
+});
+
+//BOTON PUZZLE 5
+$("#puzzle5_btn").click(function(){
+
+    $("#puzzle5").show();
+
+    $("#mapa").css("-webkit-filter", "blur(10px)");
+});
+
+//BOTON PUZZLE 6
+$("#puzzle6_btn").click(function(){
+
+    $("#puzzle6").show();
+
+    $("#mapa").css("-webkit-filter", "blur(10px)");
+});
+
+
 
