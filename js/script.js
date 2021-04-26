5cf7174eba3dfaef3deaa5a19980499e29610648
Andrea <andrellart@hotmail.com>
2021-04-26 21:29:51 +0200

    Terminar estructura puzzles y crear activación de botones puzzle 1 y 2
    He terminado la estructura de los puzzles (ahora mismo solo está aplicada en los puzzles 1 y 2, solo hay que copiarla al resto).
    Creado la activación y desactivación de los botones para acceder a los diferentes mundos y a sus respectivos puzzles (puzzle 1 y 2). Al principio solo te deja abrir el puzzle 1, cuando lo resuelves esté botón se pone en blanco y negro y te deja clickar el que viene a continuación, y así sucesivamente.

--- js/script.js
+++ js/script.js
@@ -3,13 +3,24 @@
 $("#inst").hide();
 
 $("#puzzle1").hide();
+$("#pirata_closed").hide();
+$("#puzzle1_cont").hide();
+$("#puzzle1_acierto").hide();
+$("#puzzle1_error").hide();
+
 $("#puzzle2").hide();
+$("#puzzle2_cont").hide();
+$("#puzzle2_acierto").hide();
+$("#puzzle2_error").hide();
+
+
 $("#puzzle3").hide();
 $("#puzzle4").hide();
 $("#puzzle5").hide();
 $("#puzzle6").hide();
 
-$("#puzzle1_cont").hide();
+
+
 
 
 
@@ -45,6 +56,8 @@ $("#inicio_btn1").click(function(){
 });
 
 
+
+
 // PUZZLE 1
 $("#puzzle1_btn").click(function(){
 
@@ -68,19 +81,132 @@ $("#puzzle1_btn1").click(function(){
 $("#cerrar_btn1").click(function(){
 
     $("#puzzle1").hide();
+    $("#puzzle1_cont").hide();
+    $("#puzzle1_inicio").show();
+
 
     $("#mapa").css("-webkit-filter", "blur(0px)");
 });
 
 
+    //COMPROBAR PUZZLE 1 PROVISIONAL (SIEMPRE ACIERTA) --> ACTIVAR CUANDO CORRESPONDA
+$("#puzzle1_btn2").click(function(){
 
-//BOTON PUZZLE 2
+    $("#puzzle1_acierto").show();
+
+    $("#puzzle1_cont").css("-webkit-filter", "blur(10px)");
+    $("#cerrar_btn1").css("-webkit-filter", "blur(10px)");
+});
+
+/*
+//COMPROBAR PUZZLE 1 PROVISIONAL (SIEMPRE FALLA) --> ACTIVAR CUANDO CORRESPONDA
+$("#puzzle1_btn2").click(function(){
+
+    $("#puzzle1_error").show();
+
+
+    $("#puzzle1_cont").css("-webkit-filter", "blur(10px)");
+    $("#cerrar_btn1").css("-webkit-filter", "blur(10px)");
+});
+*/
+
+    //ACERTAR PUZZLE 1
+$("#puzzle1_btn3").click(function(){
+
+    $("#puzzle1").hide();
+
+    $("#pirata_closed").show();
+    $("#narco_closed").hide();
+    
+    $("#mapa").css("-webkit-filter", "blur(0px)");
+});
+
+//FALLAR PUZZLE 1
+$("#puzzle1_btn4").click(function(){
+
+    $("#puzzle1_error").hide();
+    
+    $("#puzzle1_cont").css("-webkit-filter", "blur(0px)");
+    $("#cerrar_btn1").css("-webkit-filter", "blur(0px)");
+});
+
+
+
+//PUZZLE 2
 $("#puzzle2_btn").click(function(){
 
     $("#puzzle2").show();
 
     $("#mapa").css("-webkit-filter", "blur(10px)");
 });
+
+
+    //COMENZAR PUZZLE 2
+
+    $("#puzzle2_btn1").click(function(){
+
+        $("#puzzle2_cont").show();
+        $("#puzzle2_inicio").hide();
+        
+        
+    });
+    
+        //CERRAR PUZZLE 2
+    $("#cerrar_btn2").click(function(){
+    
+        $("#puzzle2").hide();
+        $("#puzzle2_cont").hide();
+        $("#puzzle2_inicio").show();
+    
+    
+        $("#mapa").css("-webkit-filter", "blur(0px)");
+    });
+    
+    
+        //COMPROBAR PUZZLE 2 PROVISIONAL (SIEMPRE ACIERTA) --> ACTIVAR CUANDO CORRESPONDA
+    $("#puzzle2_btn2").click(function(){
+    
+        $("#puzzle2_acierto").show();
+    
+        $("#puzzle2_cont").css("-webkit-filter", "blur(10px)");
+        $("#cerrar_btn2").css("-webkit-filter", "blur(10px)");
+    });
+    
+    /*
+    //COMPROBAR PUZZLE 2 PROVISIONAL (SIEMPRE FALLA) --> ACTIVAR CUANDO CORRESPONDA
+    $("#puzzle2_btn2").click(function(){
+    
+        $("#puzzle2_error").show();
+    
+    
+        $("#puzzle2_cont").css("-webkit-filter", "blur(10px)");
+        $("#cerrar_btn2").css("-webkit-filter", "blur(10px)");
+    });
+    */
+    
+        //ACERTAR PUZZLE 2
+    $("#puzzle2_btn3").click(function(){
+    
+        $("#puzzle2").hide();
+    
+        $("#narco_closed").show();
+        $("#narco_closed").css("-webkit-filter", "grayscale(1)");
+        
+        
+        $("#mapa").css("-webkit-filter", "blur(0px)");
+    });
+    
+    //FALLAR PUZZLE 2
+    $("#puzzle2_btn4").click(function(){
+    
+        $("#puzzle2_error").hide();
+        
+        $("#puzzle2_cont").css("-webkit-filter", "blur(0px)");
+        $("#cerrar_btn2").css("-webkit-filter", "blur(0px)");
+    });
+
+
+
 
 //BOTON PUZZLE 3
 $("#puzzle3_btn").click(function(){
